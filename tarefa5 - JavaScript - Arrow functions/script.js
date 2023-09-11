a9b21ac2d81f0ad3478ce0def494dbb51436e3ad
flaviogui <flavioglaydson-pc@hotmail.com>
2023-09-11 09:46:13 -0300

    Q1

--- tarefa5 - JavaScript - Arrow functions/script.js
+++ tarefa5 - JavaScript - Arrow functions/script.js
@@ -1,10 +1,12 @@
-let cervejas = ["Guiness", "Desperados", "Becks"]   
-
-      const carregarDiv = () => {
-         const div = document.getElementById("meu amor")
-         const cervejasHtml = cervejas.map( item => `<h1>${item}</h1>` ) 
-         div.innerHTML = `${cervejasHtml.join("\n")}`
-      }
-
-      let botao = document.getElementById("botaoCarregar")
-      botao.addEventListener("click", carregarDiv)     
+const carregarDiv = (cervs) => {
+    const div = document.getElementById("meu amor");
+    const cervejasHtml = cervs.map(item => `<h1>${item}</h1>`);
+    div.innerHTML = `${cervejasHtml.join("\n")}`;
+ }
+ 
+ let botao = document.getElementById("botaoCarregar");
+ botao.addEventListener("click", () => {
+    const cervejas = ["Guiness", "Desperados", "Becks"];
+    carregarDiv(cervejas);
+ });
+ 
