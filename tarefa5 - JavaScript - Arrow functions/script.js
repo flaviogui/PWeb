72404ac60f715fa14f01c04459f80b950a6ad795
flaviogui <flavioglaydson-pc@hotmail.com>
2023-09-14 09:30:29 -0300

    Finalizada Q2 e Q3

--- tarefa5 - JavaScript - Arrow functions/script.js
+++ tarefa5 - JavaScript - Arrow functions/script.js
@@ -1,12 +1,34 @@
-const carregarDiv = (cervs) => {
-    const div = document.getElementById("meu amor");
-    const cervejasHtml = cervs.map(item => `<h1>${item}</h1>`);
-    div.innerHTML = `${cervejasHtml.join("\n")}`;
- }
- 
- let botao = document.getElementById("botaoCarregar");
- botao.addEventListener("click", () => {
-    const cervejas = ["Guiness", "Desperados", "Becks"];
-    carregarDiv(cervejas);
- });
- 
+let sorvetes = ["Haagen Dazs", "Ben & Jerrys", "Sorvelândia"];
+
+        const carregarTabela = () => {
+            let tbody = document.querySelector("#tabelaSorvetes tbody");
+
+            let sorvetesHtml = sorvetes.map((nome) => {
+                return `<tr><td>${nome}</td></tr>`;
+            });
+
+            tbody.innerHTML = sorvetesHtml.join("");
+        }
+
+        const ordenarSorvetes = () => {
+            sorvetes.sort(); // ordena o array de cervejas em ordem alfabética
+
+            carregarTabela(); // atualiza a tabela após a ordenação
+        }
+
+        const embaralharSorvetes = () => {
+            sorvetes.sort(() => {
+                return 0.5 - Math.random(); // retorna valores aleatórios 
+            });
+
+            carregarTabela(); // atualiza a tabela após o embaralhamento
+        }
+
+        let botaoCarregar = document.getElementById("botaoCarregar");
+        botaoCarregar.addEventListener("click", carregarTabela);
+
+        let botaoOrdenar = document.getElementById("botaoOrdenar");
+        botaoOrdenar.addEventListener("click", ordenarSorvetes);
+
+        let botaoEmbaralhar = document.getElementById("botaoEmbaralhar");
+        botaoEmbaralhar.addEventListener("click", embaralharSorvetes);
