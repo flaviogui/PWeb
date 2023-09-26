7b001474f7680b014e3630c68d0924ff0ef15c57
flaviogui <flavioglaydson-pc@hotmail.com>
2023-09-26 09:47:04 -0300

    Q2

--- tarefa6 - objetos Json/script.js
+++ tarefa6 - objetos Json/script.js
@@ -1,30 +1,30 @@
 let cervejas = [
-   {
-       name: "Guiness",
-       alcohol: "10%"
-   },
-   {
-       name: "Desperados",
-       alcohol: "6%"
-   },
-   {
-       name: "Becks",
-       alcohol: "5%"
-   }
-]
-
-const carregarTabela = (cs) => {
-   const tbody = document.getElementById("cervejasBody");
-   const tabela = document.getElementById("cervejasTable");
-
-   tabela.style.display = "block"; 
-
-   const linhas = cs.map((item) => {
-       return `<tr><td>${item.name}</td><td>${item.alcohol}</td></tr>`;
-   });
-
-   tbody.innerHTML = linhas.join("");
-}
-
-let botao = document.getElementById("botaoCarregar");
-botao.addEventListener("click", () => carregarTabela(cervejas));
+    {
+        name: "Guiness",
+        alcohol: "10%"
+    },
+    {
+        name: "Desperados",
+        alcohol: "6%"
+    },
+    {
+        name: "Becks",
+        alcohol: "5%"
+    }
+ ]
+ 
+ const carregarTabela = (cs) => {
+    const tbody = document.getElementById("cervejasBody");
+    const tabela = document.getElementById("cervejasTable");
+ 
+    tabela.style.display = "block"; 
+ 
+    const linhas = cs.map((item) => {
+        return `<tr><td>${item.name}</td><td>${item.alcohol}</td></tr>`;
+    });
+ 
+    tbody.innerHTML = linhas.join("");
+ }
+ 
+ let botao = document.getElementById("botaoCarregar");
+ botao.addEventListener("click", () => carregarTabela(cervejas));
