bcd775827f5b6202a1235aefc72dcd873c88cd3d
flaviogui <flavioglaydson-pc@hotmail.com>
2023-12-11 10:04:34 -0300

    Com imagem

--- Receitas 2ª Unidade/pages/receita5/movies3.js
+++ Receitas 2ª Unidade/pages/receita5/movies3.js
@@ -1,84 +1,60 @@
-import useSWR from 'swr'
+import useSWR from 'swr';
+import { useState } from 'react';
 
-import {useState} from 'react'
+export default function Movies3() {
+  const [url, setUrl] = useState('');
+  const { data, error } = useSWR(url, theFetcher);
 
+  const onClickHandler = (e) => {
+    e.preventDefault();
+    if (url === '') setUrl('http://www.omdbapi.com/?apikey=16a58b53&s=bagdad');
+    else setUrl('');
+  };
 
-
-export default function Movies3(){
-
-    const [url, setUrl] = useState('')
-
-    const {data, error} = useSWR(url, theFetcher)
-
-
-
-    const onClickHandler = (e) => {
-
-        e.preventDefault()
-
-        if (url === '') setUrl('http://www.omdbapi.com/?apikey=16a58b53&s=bagdad')
-
-        else setUrl('')
-
-    }
-
-
-
-    return (
-
-        <div>
-
-            <TheLink url={url} handler={onClickHandler}/>
-
-            <TheMovies data={ error?{error:'Erro na pesquisa'}: data ? data: {Search:''} } show={url !== ''}/>
-
-        
-
-        </div>
-
-    )
-
+  return (
+    <div>
+      <TheLink url={url} handler={onClickHandler} />
+      <TheMovies data={error ? { error: 'Erro na pesquisa' } : data ? data : { Search: '' }} show={url !== ''} />
+    </div>
+  );
 }
-
-
 
 async function theFetcher(url) {
-    if (url === null || url === '') return {Search:''}
-    const res = await fetch(url);
-    const json = await res.json();
-    return json;
+  if (url === null || url === '') return { Search: '' };
+  const res = await fetch(url);
+  const json = await res.json();
+  return json;
 }
 
-export function TheMovies({data,show}){
+export function TheMovies({ data, show }) {
+  if (!show) return <div></div>;
 
-    if (!show) return (<div></div>)    
+  if (data.error) return <div>falha na requisição</div>;
 
-    if (data.error) return (<div>falha na requisição</div>)
+  if (data.Search === '') return <div>carregando...</div>;
 
-    if (data.Search === '' ) return (<div>carregando...</div>)
-
-    return (
-
-        <div>
-
-            { data.Search.map( (m) => <div>{m.Title} --- {m.Year}</div>  ) }            
-
+  return (
+    <div>
+      {data.Search.map((m) => (
+        <div key={m.imdbID}>
+          <p></p>
+          <img src={m.Poster} alt={m.Title} width="100px" />
+          <p></p>
+          <div>
+            {m.Title} --- {m.Year}
+          </div>
         </div>
-
-    )
-
+      ))}
+    </div>
+  );
 }
 
-export function TheLink({url, handler}){    
-
-    return (
-
-        <div>
-
-            <a href="/movies3.js" onClick={handler}> {url === '' ? 'Mostrar' : 'Ocultar'} </a>
-
-        </div>
-
-    )
-
+export function TheLink({ url, handler }) {
+  return (
+    <div>
+      <a href="/movies3.js" onClick={handler}>
+        {url === '' ? 'Mostrar' : 'Ocultar'}
+      </a>
+    </div>
+  );
 }
